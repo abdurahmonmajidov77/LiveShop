1334d98a148ed7431bd1ec970400775b3fbefe51
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-01-25 21:06:45 +0500

    hi

--- liveshop-codes/src/components/NavbarComponent/index.jsx
+++ liveshop-codes/src/components/NavbarComponent/index.jsx
@@ -1,11 +1,19 @@
-import NavLogo from '../../assets/Logo.png';
 import './style.css'
 
 function NavbarComponent() {
   return (
     <div className="NavbarComponent">
         <div className="container">
-            <img src={NavLogo} alt="" />
+            <div className="NavBox">
+                <img src="https://res.cloudinary.com/dccxaewos/image/upload/v1674659800/assets/LiveShop_gbuqte.png" alt="" />
+                <input type="text" placeholder='Search...'/>
+                <ul>
+                    <li>K</li>
+                    <li>K</li>
+                    <li>K</li>
+                    <li>K</li>
+                </ul>
+            </div>
         </div>
     </div>
   );
